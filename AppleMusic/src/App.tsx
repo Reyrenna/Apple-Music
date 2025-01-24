bf78ee3aae52dd39d1d87e487e499600a3516d96
Reyett___ <reyrenna4@gmail.com>
2025-01-24 17:24:44 +0100

    ops

--- AppleMusic/src/App.tsx
+++ AppleMusic/src/App.tsx
@@ -1,24 +1,22 @@
-import { Row, Col } from 'react-bootstrap'
-import './App.css'
-import NavApple from './components/nav'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './components/home';
-
+import { Row, Col } from "react-bootstrap";
+import "./App.css";
+import NavApple from "./components/nav";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/home";
 
 function App() {
-
   return (
     <>
-    <Row>
-      <Col md={3}>
-      <NavApple></NavApple>
-      </Col>
-      <Col md={9} xs={12}>
-    <Home/>
-      </Col>
-    </Row>
+      <Row>
+        <Col md={3}>
+          <NavApple></NavApple>
+        </Col>
+        <Col md={9} xs={12}>
+          <Home />
+        </Col>
+      </Row>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- AppleMusic/src/components/home.tsx
+++ AppleMusic/src/components/home.tsx
@@ -1,6 +1,6 @@
 import Esplora from "./altroDaEsplorare";
-import NuoveUscite from "./canzoniNuove";
 import CardGrandi from "./cardGrandi";
+import Fetch from "./fetchSong";
 import AppleNav from "./navrespons";
 import NuoviEpisodi from "./nuoviEpisodiRadio";
 function Home() {
@@ -9,7 +9,7 @@ function Home() {
       <AppleNav />
       <CardGrandi />
       <NuoviEpisodi />
-      <NuoveUscite />
+      <Fetch />
       <Esplora />
     </>
   );
