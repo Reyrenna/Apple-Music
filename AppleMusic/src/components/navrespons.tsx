254b46e17ac7b7c34980f591c68eca8100ce2ed8
Reyett___ <reyrenna4@gmail.com>
2025-01-24 17:41:51 +0100

    smartphone

--- AppleMusic/src/components/navrespons.tsx
+++ AppleMusic/src/components/navrespons.tsx
@@ -1,19 +1,35 @@
-import {Container, Nav, Navbar} from 'react-bootstrap'
-import LogoApple from '/apple.svg'
+import { Nav, Navbar, Col, Row } from "react-bootstrap";
+import LogoApple from "/apple.svg";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 function AppleNav() {
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
-        <Container fluid>
-          
-          <Nav className="me-auto d-flex justify-content-between">
-            <Nav.Link href="#"></Nav.Link>
-            <Navbar.Brand href="#"><img src={LogoApple} alt='logo apple'></img></Navbar.Brand>
-            <Nav.Link href="#" className=' text-danger'>Accedi</Nav.Link>
+        <Row className=" w-100">
+          <Nav className="me-auto">
+            <Col xs={4}>
+              <Nav.Link
+                href="#"
+                className="text-start ps-4 text-danger fs-3 altezza"
+              >
+                <HiOutlineMenuAlt3 />
+              </Nav.Link>
+            </Col>
+            <Col xs={4}>
+              <Navbar.Brand href="#">
+                <img src={LogoApple} alt="logo apple"></img>
+              </Navbar.Brand>
+            </Col>
+            <Col xs={4}>
+              <Nav.Link href="#" className=" text-end text-danger">
+                Accedi
+              </Nav.Link>
+            </Col>
           </Nav>
-        </Container>
+        </Row>
       </Navbar>
-      </>
-  )}
+    </>
+  );
+}
 
-  export default AppleNav
+export default AppleNav;
